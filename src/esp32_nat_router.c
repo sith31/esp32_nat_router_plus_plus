09ac07c1f3ae75ccca71a73dd983d79bb76e72ce
sith31 <pepe_pika@hotmail.com>
2026-02-08 12:44:25 +0100

    Implement SD card initialization function
    Added SD card initialization function to mount the SD card with specified configurations.

--- src/esp32_nat_router.c
+++ src/esp32_nat_router.c
@@ -17,6 +17,9 @@
 #include "router_handler.h"
 #include "wifi_init.h"
 #include "ota_handler.h"
+#include "esp_vfs_fat.h"
+#include "sdmmc_cmd.h"
+#include "driver/sdmmc_host.h"
 
 #if !IP_NAPT
 #error "IP_NAPT must be defined"
@@ -31,11 +34,34 @@ uint32_t my_ap_ip;
 
 static const char *TAG = "ESP32 NAT router +";
 
+void init_sd_card() {
+    esp_vfs_fat_sdmmc_mount_config_t mount_config = {
+        .format_if_mount_failed = true,
+        .max_files = 5,
+        .allocation_unit_size = 16 * 1024
+    };
+    sdmmc_host_t host = SDMMC_HOST_DEFAULT();
+    sdmmc_slot_config_t slot_config = SDMMC_SLOT_CONFIG_DEFAULT();
+    
+    // Pines para ESP32-S3 (Ajusta si tu placa usa otros)
+    slot_config.width = 1; 
+    slot_config.clk = GPIO_NUM_39; 
+    slot_config.cmd = GPIO_NUM_38;
+    slot_config.d0  = GPIO_NUM_40;
+
+    ESP_LOGI("SD", "Montando tarjeta SD...");
+    esp_err_t ret = esp_vfs_fat_sdmmc_mount("/sdcard", &host, &slot_config, &mount_config, NULL);
+    if (ret != ESP_OK) {
+        ESP_LOGE("SD", "Error al montar SD (%s)", esp_err_to_name(ret));
+    }
+}
+
 
 //-----------------------------------------------------------------------------
 void app_main(void)
 {
     initialize_nvs();
+    init_sd_card();
 
 #if CONFIG_STORE_HISTORY
     initialize_filesystem();
